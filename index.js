f8ddbf9e627d344a7108d082c0b0ed1d21d4b993
Shaan2170 <shaanmustafa1234@gmail.com>
2025-08-09 12:55:29 +0530

    hasehd,try catch

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+const bcrypt = require("bcrypt");
 const express = require("express");
 const { UserModel, TodoModel } = require("./db");
 const { auth, JWT_SECRET } = require("./auth");
 const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
 
-mongoose.connect("")
+mongoose.connect("mongodb+srv://admin:uidF8q9aBMDzD7s3@cluster0.pbhnvrn.mongodb.net/  ")
 
 const app = express();
 app.use(express.json());
@@ -14,12 +15,23 @@ app.post("/signup", async function(req, res) {
     const password = req.body.password;
     const name = req.body.name;
 
+    let errorThrown = false;
+    try{
+    const hashedPassword = await bcrypt.hash(password,5);
+    console.log(hashedPassword);
+
     await UserModel.create({
         email: email,
         password: password,
         name: name
     });
-    
+} catch(e){
+     res.json({
+        message: "User already exists"
+    })
+    errorThrown = true;
+}
+    if(!errorThrown)
     res.json({
         message: "You are signed up"
     })
@@ -31,11 +43,19 @@ app.post("/signin", async function(req, res) {
     const password = req.body.password;
 
     const response = await UserModel.findOne({
-        email: email,
-        password: password,
+        email: email
     });
 
-    if (response) {
+    if(!response){
+        res.status(403).json({
+            message:"User does not exist in our db"
+        })
+        return
+    }
+
+    const passwordMatch = await bcrypt.compare(password,response.password);
+
+    if (passwordMatch) {
         const token = jwt.sign({
             id: response._id.toString()
         }, JWT_SECRET);
